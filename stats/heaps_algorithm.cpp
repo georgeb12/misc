6416e4bfae4bd89697756bb9a370b1b3a57735f5
pegeler <paulegeler@gmail.com>
2017-12-02 12:48:43 -0500

    Added heaps_algorithm.cpp

---
+++ stats/heaps_algorithm.cpp
@@ -0,0 +1,58 @@
+// C++ program to print all permutations using
+// Heap's algorithm
+// http://www.geeksforgeeks.org/heaps-algorithm-for-generating-permutations/
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+ 
+//Prints the array
+void printArr(int a[],int n)
+{
+    for (int i=0; i<n; i++)
+        cout << a[i] << " ";
+    printf("\n");
+}
+ 
+// Generating permutation using Heap Algorithm
+void heapPermutation(int a[], int size, int n)
+{
+    // if size becomes 1 then prints the obtained
+    // permutation
+    if (size == 1)
+    {
+        printArr(a, n);
+        return;
+    }
+ 
+    for (int i=0; i<size; i++)
+    {
+        heapPermutation(a,size-1,n);
+ 
+        // if size is odd, swap first and last
+        // element
+        if (size%2==1)
+            swap(a[0], a[size-1]);
+ 
+        // If size is even, swap ith and last
+        // element
+        else
+            swap(a[i], a[size-1]);
+    }
+}
+ 
+// Driver code
+int main(int argc, char *argv[])
+{
+    int n;
+    sscanf(argv[1], "%i", &n);
+    
+    int a[n-1];
+        
+    for (int i=0; i<n; i++) 
+    {
+        a[i] = i + 1;
+    }
+    
+    heapPermutation(a, n, n);
+    return 0;
+}
